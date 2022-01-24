164bc3b35ed6c9a0e343bba278b91f4753d6e570
Alex <cyalexlau@yahoo.com.hk>
2022-01-24 20:06:59 +0800

    Initial commit

---
+++ Tuple.js
@@ -0,0 +1,8 @@
+export class Tuple {
+	constructor(x, y, z, w) {
+		this._x = x;
+		this._y = y;
+		this._z = z;
+		this._w = w;
+	}
+}
---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+
+import { Tuple } from './Tuple';
+
+const tuple = new Tuple(0,0,0,0);
+
+console.log(tuple);
